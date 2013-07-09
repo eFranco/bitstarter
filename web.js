84b4708767d1e44966b40122e84286e696dd2795
eFranco <enrique_franco_pedroso@yahoo.es>
2013-07-09 17:16:55 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -2,6 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+
 app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toStirng());
 });
@@ -10,3 +13,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
